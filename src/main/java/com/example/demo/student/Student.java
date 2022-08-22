be6dd7d9fb679e49ab3ad3eb72e9cac70380f122
Berke Baklan <berkebaklan@hotmail.com>
2022-08-22 18:14:17 +0300

    First commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,69 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfBirth;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, Integer age, LocalDate dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Student(String name, String email, Integer age, LocalDate dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
